5c6c65e7b62ed2856bd4514f488a96c5e035812f
Hermione Allognon <allognonhermione@gmail.com>
2022-06-15 10:10:19 +0200

    first commit

---
+++ .history/src/main_20220615100319.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
